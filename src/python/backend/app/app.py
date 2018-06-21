ddb87e92cbf8693988ecb3a10c6bfe7953da5098
leochen <leo@leochens-MacBook-Pro.local>
2018-06-21 20:53:30 +0800

    Changed by leo

--- src/python/backend/app/app.py
+++ src/python/backend/app/app.py
@@ -2,9 +2,6 @@
 import connexion
 app = connexion.App(__name__, specification_dir='../swagger/')
 
-def post_greeting(name: str) -> str:
-    return 'Hello {name}'.format(name=name)
-
 app.add_api('swagger.yaml', arguments={'title': 'AlphaCar Blockchain API Service'})
 
 if __name__ == '__main__':
---
+++ src/python/backend/app/controller/index.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+def post_greeting(name: str) -> str:
+    return 'Hello {name}'.format(name=name)
+    

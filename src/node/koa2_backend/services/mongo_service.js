309e90e252d37f90ccd43699167cbd737d95863f
leochen <leo@leochens-MacBook-Pro.local>
2018-07-06 15:37:27 +0800

    Changed by leo

--- src/node/koa2_backend/services/mongo_service.js
+++ src/node/koa2_backend/services/mongo_service.js
@@ -64,14 +64,27 @@ export async function getUBIInfoByCode(ctx) {
   ctx.response.body = body
 }
 
-export async function getUBIInfoList(ctx) {
+export function gen_search_cond(ctx) {
+
   var param = ctx.request.query;
   let search_type = param['search_type']
   let search_txt = param['search_txt']
+  let page = parseInt(param['page'])
+  let page_size = parseInt(param['page_size'])
+
+  //console.log('page:', page, ' page_size:', page_size)
 
   let cond = {}
 
   var opt_lst = ['1', '2', '3', '4'];
+
+  if (page == undefined || isNaN(page)) {
+    page = -1
+  }
+
+  if (page_size == undefined || isNaN(page_size)) {
+    page_size = -1
+  }
 
   if (search_type == undefined || search_type == '' || opt_lst.indexOf(search_type) == -1) {
     search_type = '0'
@@ -114,14 +127,33 @@ export async function getUBIInfoList(ctx) {
     }
   }
 
+  return { 'cond': cond, 'page': page, 'page_size': page_size }
+}
+
+export async function getUBIInfoList(ctx) {
+
+  let cond = gen_search_cond(ctx)
+
   console.log('cond', cond)
 
-  let res = await mongowrapper.UBIInfoModel.find(cond)
+  let res;
+  let total_count = 0;
+  
+  if (cond['page'] == -1 || cond['page_size'] == -1) {
+    res = await mongowrapper.UBIInfoModel.find(cond['cond'])
     .sort('-timestamp')
     .select({
       _id: 0
-    })
-    .exec()
+    }).exec();
+    total_count = res.length;
+  } else {
+    res = await mongowrapper.UBIInfoModel.find(cond['cond'])
+    .sort('-timestamp')
+    .select({
+      _id: 0
+    }).skip(cond['page'] * cond['page_size']).limit(cond['page_size']).exec();
+    total_count = await mongowrapper.UBIInfoModel.count(cond['cond']).exec();
+  }
 
   let error_code = 0;
   let error_msg = '';
@@ -129,7 +161,10 @@ export async function getUBIInfoList(ctx) {
   let body = {
     'error_code': error_code,
     'error_msg': error_msg,
-    data: res
+    data: {
+    'total_count': total_count,
+    'lst': res
+    }
   }
 
   ctx.response.body = body

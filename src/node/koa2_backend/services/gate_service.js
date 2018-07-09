6bed438ef3671ee8bc4cdfbc4d0b3334fffb0530
leochen <leo@leochens-MacBook-Pro.local>
2018-07-09 16:55:26 +0800

    Changed by leo

--- src/node/koa2_backend/services/gate_service.js
+++ src/node/koa2_backend/services/gate_service.js
@@ -7,18 +7,34 @@ export async function getPrice(ctx) {
   await gate_axios.service.get(gate_axios.TICKER_URL + param)
     .then(function (response) {
       if (response.status == 200) {
-        console.log('response.data:', response.data)
-        console.log('last:', response.data.last)
-        let price = parseFloat(response.data.last)
-        let result = {
+        let quoteVolume = parseFloat(response.data.quoteVolume)
+        let baseVolume = parseFloat(response.data.baseVolume)
+        let highestBid = parseFloat(response.data.highestBid)
+        let high24hr = parseFloat(response.data.high24hr)
+        let last = parseFloat(response.data.last)
+        let lowestAsk = parseFloat(response.data.lowestAsk)
+        let elapsed = response.data.elapsed
+        let result = response.data.last == "true" ? true : false;
+        let low24hr = parseFloat(response.data.low24hr)
+        let percentChange = parseFloat(response.data.percentChange)
+        let _result = {
           'error_code': 0,
           'error_msg': '',
           data: {
-            'price': price
+            'quoteVolume': quoteVolume,
+            'baseVolume': baseVolume,
+            'highestBid': highestBid,
+            'high24hr': high24hr,
+            'last': last,
+            'lowestAsk': lowestAsk,
+            'elapsed': elapsed,
+            'result': result,
+            'low24hr': low24hr,
+            'percentChange': percentChange,
           }
         }
-        console.log('result:', result)
-        ctx.response.body = result
+        console.log('result:', _result)
+        ctx.response.body = _result
       } else {
         ctx.response.body = {
           'error_code': -1,

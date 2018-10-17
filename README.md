2ca5db6a149ed340023c47b3a480809ac2f3f295
leochan007 <leochan007@163.com>
2018-10-17 11:40:31 +0800

    Changed by leo

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Dependencies:
 
 pip install ipfsapi
+
+sudo docker ps -a | awk -F ' ' '{print $1}' |xargs sudo docker stop && echo y | sudo docker system prune
--- docker/nginx/cp_dist.sh
+++ docker/nginx/cp_dist.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-dist=../../../alphacar-browser/dist
+dist_root=../../../alphacar-browser
+dist=$dist_root/dist
 
 if [ -n "$1" ]; then
     dist=$1
 fi
 
 rm -rf dist
+
+if [ ! -d $dist ]; then
+    cur_d=`pwd`
+    echo 'cur_d:'$cur_d
+    cd $dist_root
+    npm run build
+    cd $cur_d
+fi
+
 cp -rf $dist dist
